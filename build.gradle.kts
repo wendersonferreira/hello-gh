9a27def843df9e7a81d9521bcff78bb2b4ab2404
Wenderson Ferreira de Souza <wenderson@trustsystems.com.br>
2021-02-06 23:44:22 +0100

    github actinos support

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,13 @@ dependencies {
     testImplementation("junit:junit:4.13.1")
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+        vendor.set(JvmVendorSpec.BELLSOFT)
+    }
+}
+
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
